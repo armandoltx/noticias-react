0639e653167627b386c396400185727bafc4a38e
armandoltx <armando@armandoltx.com>
2019-05-16 18:21:20 +1000

    Fetch y Anadidas las noticias al state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './componentes/Header';
-function App() {
-  return (
-    <div className="App">
-      <Header titulo={"Noticias"} />
-    </div>
-  );
+
+
+class App extends Component {
+
+  state = {
+    noticias: [],
+  }
+
+  componentDidMount() {
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = () => {
+    console.log("consultarNoticias");
+    // leer la url y agregar el API key
+    //https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=dfe2d931c07b4e1f8b2fe27eaa49e40d
+    const appId = 'dfe2d931c07b4e1f8b2fe27eaa49e40d';
+    let url = `https://newsapi.org/v2/top-headlines?country=au&category=general&apiKey=${appId}`;
+
+
+    // query con fetch api
+    fetch(url)
+      .then(respuesta => {
+        console.log("respuesta dentro de fetch ", fetch);
+        return respuesta.json();
+      })
+      .then(noticias => {
+        console.log("noticias en fetch ", noticias);
+        this.setState({
+          noticias: noticias.articles,
+        })
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Header titulo={"Noticias"} />
+      </div>
+    );
+  }
 }
 
 export default App;
