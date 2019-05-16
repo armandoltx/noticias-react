82fabad594174b8f4966bb7056dd5b234b0bb09a
armandoltx <armando@armandoltx.com>
2019-05-16 18:45:12 +1000

    Creado el componente noticia

---
+++ src/componentes/Noticia.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Noticia = () => {
+  return (
+    <div>
+      Hola Desde NOTICIA sin S
+    </div>
+  );
+};
+
+export default Noticia;
--- src/componentes/Noticias.js
+++ src/componentes/Noticias.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Noticia from './Noticia';
 
 class Noticias extends Component {
   render() {
     return (
       <div className="row">
         hola desde noticias.js
+        <Noticia />
     </div>
     );
   }
