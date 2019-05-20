7ba62d53845b574aea7b9a2cd3ab4e301c174489
Armando Ruiz <armando.ruiz@industrie.co>
2019-05-20 11:01:14 +1000

    Mostrando las noticias en el componente Noticia

--- src/componentes/Noticia.js
+++ src/componentes/Noticia.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 const Noticia = ({noticia}) => { // si aqui ponemos props
-  const {urlToImage, url, title, descrition, source} = noticia; // esto seria igual a props.noticia
-  console.log(title);
+  console.log("Noticias ", {noticia});
+  const {urlToImage, url, title, description, source} = noticia; // esto seria igual a props.noticia
+  // console.log(title);
   return (
-    <div>
-      Hola Desde NOTICIA sin S
+    <div className="col s12 m6 l4">
+      <div className="card">
+        <div className="card-image">
+          <img src={urlToImage} alt={title}/>
+          <span className="card-title">{source.name}</span>
+        </div>
+        <div className="card-content">
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+        <div className="card-action">
+          <a href={url} target="_blank" className="waves-effects waves-light btn">
+            Leer
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
